459334d4a4da7304c57959744a8c8f54749ca1b9
Astha Negi <asthanegi539@gmail.com>
2022-09-02 09:50:59 +0530

    Create 1365. How Many Numbers Are Smaller Than the Current Number.java

---
+++ Easy/1365. How Many Numbers Are Smaller Than the Current Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int ans[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            int num = nums[i];
+            
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]<num){
+                    count++;
+                }
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+}
