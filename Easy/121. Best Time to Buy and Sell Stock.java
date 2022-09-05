f30ddcff9036b7dad5fbe9846ed66c47784febef
Astha Negi <asthanegi539@gmail.com>
2022-09-05 16:46:04 +0530

    Update 121. Best Time to Buy and Sell Stock.java

--- Easy/121. Best Time to Buy and Sell Stock.java
+++ Easy/121. Best Time to Buy and Sell Stock.java
@@ -1,18 +1,30 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int small=prices[0],j=0,pro=0;
+//         for(int i=0;i<prices.length;i++)
+//         {
+//             if(prices[i]<small)
+//             {
+//                 small = prices[i];
+//             }
+//             j = prices[i]-small;
+//             if(j>pro)
+//             {
+//                 pro=j;
+//             }
+//         }
+//         return pro;
+//     }
+// }
+
 class Solution {
     public int maxProfit(int[] prices) {
-        int small=prices[0],j=0,pro=0;
-        for(int i=0;i<prices.length;i++)
-        {
-            if(prices[i]<small)
-            {
-                small = prices[i];
-            }
-            j = prices[i]-small;
-            if(j>pro)
-            {
-                pro=j;
-            }
+        int min=Integer.MAX_VALUE;
+        int max=0;
+        for(int i=0;i<prices.length;i++){
+            min=Math.min(min,prices[i]);
+            max=Math.max(max,prices[i]-min);
         }
-        return pro;
+        return max;
     }
 }
