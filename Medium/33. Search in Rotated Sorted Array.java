29c38520c9266f7778ba92c434a7a567e537878f
Astha Negi <asthanegi539@gmail.com>
2022-09-19 23:03:50 +0530

    Create 33. Search in Rotated Sorted Array.java

---
+++ Medium/33. Search in Rotated Sorted Array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i=0,n=nums.length-1;
+        while(i<=n){
+            int mid=i+(n-i)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[i]<=nums[mid]){
+                if(target>=nums[i] && target<=nums[mid]){
+                    n=mid-1;
+                } 
+                else
+                    i=mid+1;
+            }
+            else
+            {
+                if(target>=nums[mid] && target<=nums[n]){
+                    i=mid+1;
+                }
+                else
+                    n=mid-1;
+            }
+        }
+        return -1;
+    }
+}
