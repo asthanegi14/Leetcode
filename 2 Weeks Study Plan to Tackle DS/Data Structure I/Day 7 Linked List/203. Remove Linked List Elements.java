aab3ff4387e7a451b691390f37eee3d88fb3a138
Astha Negi <asthanegi539@gmail.com>
2023-03-06 10:54:50 +0530

    Create 203. Remove Linked List Elements.java

---
+++ 2 Weeks Study Plan to Tackle DS/Data Structure I/Day 7 Linked List/203. Remove Linked List Elements.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head==null){
+            return head;
+        }
+        while(head!=null && head.val==val){
+            head = head.next;
+        }
+
+        ListNode temp = head;
+        while(temp!=null && temp.next!=null){
+            if(temp.next.val==val){
+                temp.next = temp.next.next;
+            }
+            else{
+                temp = temp.next;
+            }
+        }
+        return head; 
+    }
+}
