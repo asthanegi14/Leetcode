2315de15f7f49ad52cccd25302d3629b1f275aa9
Astha Negi <asthanegi539@gmail.com>
2023-10-17 17:39:19 +0530

    Added solution - LeetHub

---
+++ Transitive closure of a Graph - GFG/transitive-closure-of-a-graph.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            int graph[][] = new int[N][N];
+            
+            for(int i = 0;i < N;i++)
+                {String a[] = in.readLine().trim().split("\\s+");
+                for(int j=0;j<N;j++)
+                graph[i][j] = Integer.parseInt(a[j]);}
+            
+            Solution ob = new Solution();
+            ArrayList<ArrayList<Integer>> ans = ob.transitiveClosure(N, graph);
+            for(int i = 0;i < N;i++)
+               { for(int j = 0;j < ans.get(i).size();j++)
+                    System.out.print(ans.get(i).get(j) + " ");
+            System.out.println();}
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static ArrayList<ArrayList<Integer>> transitiveClosure(int N, int graph[][]) {
+        
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            ans.add(new ArrayList<>());
+            for (int j = 0; j < N; j++) {
+                ans.get(i).add(0); 
+            }
+        }
+        
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == j) {
+                        graph[i][j] = 1;
+                    }
+                    if (graph[i][j] == 0 && graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                ans.get(i).set(j, graph[i][j]);
+            }
+        }
+
+        return ans;
+    }
+}
