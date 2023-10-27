063d17db8fc210b3069e58838d79e3c14ddde861
Astha Negi <asthanegi539@gmail.com>
2023-10-27 22:10:14 +0530

    Added solution - LeetHub

---
+++ Minimum Deletions - GFG/minimum-deletions.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String S = read.readLine();
+
+            Solution ob = new Solution();
+            
+            System.out.println(ob.minimumNumberOfDeletions(S));
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int minimumNumberOfDeletions(String S) {
+        //your code here
+        int n = S.length();
+        int[][] dp = new int[n+1][n+1];
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(S.charAt(i-1)==S.charAt(n-j)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return n-dp[n][n];
+    }
+}
